7a957a98cdb018ead15e3a841dad61cc299976b7
juanvallejo <juan.vallejo.12@cnu.edu>
2016-02-27 03:24:28 -0500

    mark blank cells

--- controller.py
+++ controller.py
@@ -12,25 +12,25 @@ height = 16
 mat_game = []
 
 # create 1D array containing game cells
-def init_mat(mat_game, width, height):
-	mat_game = [(1 if rand.randint(0, 100) < 20 else 0) for x in range(width * height)]
-	return mat_game
+def init_mat(matrix, width, height):
+	matrix = [(1 if rand.randint(0, 100) < 20 else 0) for x in range(width * height)]
+	return matrix
 	
-def draw_grid(n_win, mat_game, width, height):
+def draw_grid(n_win, matrix, width, height):
 	count = 0
-#	n_win.clear()
-	for i in mat_game:
+	n_win.clear()
+	for i in matrix:
 		count += 1
-#		n_win.addch(' ')
-#		n_win.addch(str(i) if i == 1 else ' ')
-		print (str(i) if i == 1 else 'o'),
-#		n_win.addch(' ')
+		n_win.addch(' ')
+		n_win.addch(str(i) if i == 1 else 'o')
+#		print (str(i) if i == 1 else 'o'),
+		n_win.addch(' ')
 		if count % height == 0:
-#			n_win.addch('\n')
-			print '\n'
-#	n_win.refresh()
+			n_win.addch('\n')
+#			print '\n'
+	n_win.refresh()
 		
-def calc_grid(mat_game, width, height):
+def calc_grid(matrix, width, height):
 
 
 	for i in range(width * height):
@@ -39,71 +39,62 @@ def calc_grid(mat_game, width, height):
 		
 		# check top neighbor
 		if int(i / height) > 0:
-			if mat_game[int((i / height - 1) + (i % height))] == 1:
+			if matrix[int((i / height - 1) + (i % height))] == 1:
 				n_cells += 1
 			
 			# check top left neighbor
-			if int(i % height) > 0 and mat_game[int((i / height - 1) + ((i % height) - 1))] == 1:
+			if int(i % height) > 0 and matrix[int((i / height - 1) + ((i % height) - 1))] == 1:
 				n_cells += 1	
 			# check top right neighbor
-			if int(i % height) < width - 1 and mat_game[int((i / height - 1) + ((i % height) + 1))] == 1:
+			if int(i % height) < width - 1 and matrix[int((i / height - 1) + ((i % height) + 1))] == 1:
 				n_cells += 1
 						
 		# check bottom neighbor
 		if int(i / height) < width - 1:
-			if mat_game[int((i / height + 1) + (i % height))] == 1:
+			if matrix[int((i / height + 1) + (i % height))] == 1:
 				n_cells += 1
 			
 			# check bottom left neighbor
-			if int(i % height) > 0 and mat_game[int((i / height + 1) + ((i % height) - 1))] == 1:
+			if int(i % height) > 0 and matrix[int((i / height + 1) + ((i % height) - 1))] == 1:
 				n_cells += 1			
 			# check bottom right neighbor
-			if int(i % height) < height - 1 and mat_game[int((i / height + 1) + ((i % height) + 1))] == 1:
+			if int(i % height) < height - 1 and matrix[int((i / height + 1) + ((i % height) + 1))] == 1:
 				n_cells += 1
 
 		# check left neighbor
 		if int(i % height) > 0:
-			if mat_game[int((i / height) + ((i % height) - 1))] == 1:
+			if matrix[int((i / height) + ((i % height) - 1))] == 1:
 				n_cells += 1	
 		# check right neighbor
 		if int(i % height) < height - 1:
-			if mat_game[int((i / height) + ((i % height) + 1))] == 1:
+			if matrix[int((i / height) + ((i % height) + 1))] == 1:
 				n_cells += 1
 
 
-		if mat_game[int((i / height) + (i % height))] == 0 and n_cells == 3:
-			mat_game[int((i / height) + (i % height))] = 1
+		if matrix[int((i / height) + (i % height))] == 0 and n_cells == 3:
+			matrix[int((i / height) + (i % height))] = 1
 		if n_cells < 2 or n_cells > 3:
-			mat_game[int((i / height) + (i % height))] = 0
+			matrix[int((i / height) + (i % height))] = 0
+
+		#matrix[int((i / height) + ((i % height) + 1))] = 1
 	
-	return mat_game
+	return matrix
 
 # init curses
-'''stdscr = curses.initscr()
+stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak()
 stdscr.keypad(1)
 n_win = curses.newwin(50, 50, 0, 0)
-'''
 
-mat_game = init_mat(mat_game, width, height)
-'''
+m_game = init_mat(mat_game, width, height)
 while(True):
-	mat_game = calc_grid(mat_game, width, height)
-	draw_grid(n_win, mat_game, width, height)
+	m_game = calc_grid(m_game, width, height)
+	draw_grid(n_win, m_game, width, height)
 	time.sleep(0.1)
-'''
-
-draw_grid(n_win, mat_game, width, height)
-print '----------------'
-mat_game2 = calc_grid(mat_game, width, height)
-draw_grid(n_win, mat_game2, width, height)
-print '----------------'
-mat_game3 = calc_grid(mat_game2, width, height)
-draw_grid(n_win, mat_game3, width, height)
 
 # end curses
-'''curses.nocbreak()
+curses.nocbreak()
 stdscr.keypad(0)
 curses.echo()
-curses.endwin()'''
+curses.endwin()
